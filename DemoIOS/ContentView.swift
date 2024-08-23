613d438743e5142548c15242b9b801a397330bb3
Thiago Ferreira <thiagofp707@gmail.com>
2024-08-23 13:21:47 -0300

    Commit Inicial

---
+++ DemoIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoIOS
+//
+//  Created by Thiago Ferreira Peixoto on 23/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoIOS/DemoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoIOSApp.swift
+//  DemoIOS
+//
+//  Created by Thiago Ferreira Peixoto on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
